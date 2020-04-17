8795b523bf1ad2632606c5c7f0d2c0ed227024c6
Anthony Shew <anthonyshew@gmail.com>
2020-04-16 23:10:20 -0700

    First draft of homepage.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react"
+import React from "react"
 import { Link } from "gatsby"
 import '../styles/reset.scss'
 import '../styles/global.scss'
@@ -6,11 +6,7 @@ import '../styles/navbar.scss'
 import '../styles/footer.scss'
 
 import { useStaticQuery, graphql } from "gatsby"
-import useBodyScrollLock from '../hooks/useBodyScrollLock'
-import Image from "gatsby-image"
 import Logo from "../svg/logo.svg"
-import Hamburger from '../svg/hamburger.svg'
-import Xburger from '../svg/xburger.svg'
 
 const Layout = ({ location, children }) => {
   const data = useStaticQuery(graphql`
@@ -40,19 +36,10 @@ const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   let header
 
-  const navlinks = [
-    ["/dear-players", "Dear Players"],
-    ["/dear-sponsors", "Dear Sponsors"],
-    ["/about-us", "About Us"],
-    ["/blog", "Blog"],
-    [data.site.siteMetadata.donate.shop, "Shop"],
-    ["/donate", "Donate"]
-  ]
-
   if (location.pathname === rootPath) {
     header = (
       <section className="container-home-hero">
-        <h1>YOUR SITE NEEDS A HERO</h1>
+        <h2>YOUR SITE NEEDS A HERO</h2>
         <Logo />
       </section>
     )
@@ -70,178 +57,6 @@ const Layout = ({ location, children }) => {
 }
 
 export default Layout
-
-const IndexNav = ({ data, navlinks }) => (
-  <nav className="navbar full">
-    <span className="logo">
-      <Link to="/">
-        <Image
-          className="site-logo"
-          fixed={data.shewLogo.childImageSharp.fixed}
-          alt={data.site.siteMetadata.title}
-        />
-      </Link>
-    </span>
-    <span className="link-list">
-      {navlinks.map((elem, index) => {
-        if (elem[0].startsWith("http")) {
-          return <div key={elem[0]} className="link-container">
-            <a href={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`} target="_blank" rel="noopener noreferrer">
-              {elem[1]}
-            </a>
-          </div>
-        } else {
-          return <div key={elem[0]} className="link-container">
-            <Link to={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`}>
-              {elem[1]}
-            </Link>
-          </div>
-        }
-      })}
-    </span>
-  </nav>
-)
-
-const PageNav = ({ data, navlinks }) => (
-  <nav className="navbar full page-nav">
-    <span className="logo">
-      <Link to="/">
-        <Image
-          className="site-logo"
-          fixed={data.shewLogo.childImageSharp.fixed}
-          alt={data.site.siteMetadata.title}
-        />
-      </Link>
-    </span>
-    <span className="link-list">
-      <Link to="/" className="link">
-        Home
-          </Link>
-      {navlinks.map((elem, index) => {
-        if (elem[0].startsWith("http")) {
-          return <a key={elem[0]} href={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`} target="_blank" rel="noopener noreferrer">
-            {elem[1]}
-          </a>
-        } else {
-          return <Link key={elem[0]} to={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`}>
-            {elem[1]}
-          </Link>
-        }
-      })}
-    </span>
-  </nav>
-)
-
-const SmallDisplayNav = ({ data, navlinks }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const handleKeyboardOpen = (e) => {
-    if (e.key === "Enter") setIsOpen(true)
-  }
-
-  return (
-    <nav className="navbar mobile-navbar">
-      <span className="logo">
-        <Link to="/">
-        </Link>
-      </span>
-      <span className="hamburger-container">
-        <Hamburger
-          tabIndex={0}
-          onKeyDown={handleKeyboardOpen}
-          onClick={() => setIsOpen(true)}
-        />
-      </span>
-      {isOpen && <MobileMenu setIsOpen={setIsOpen} data={data} navlinks={navlinks} />}
-    </nav >
-  )
-}
-
-const MobileMenu = ({ setIsOpen, data, navlinks }) => {
-  useBodyScrollLock()
-  const container = useRef()
-  const first = useRef()
-  const last = useRef()
-  const shifted = useRef(false)
-
-  const handleClose = useCallback((e) => {
-    container.current.classList.add("out")
-    setTimeout(() => { setIsOpen(false) }, 250)
-  }, [setIsOpen])
-
-  const handleKeyUp = (e) => {
-    if (e.key === "Shift") shifted.current = false
-  }
-
-  useEffect(() => {
-    const background = container.current
-    const lastLink = last.current
-    const handleKeyDown = (e) => {
-      if (e.key === "Shift") shifted.current = true
-      if (e.key === "Escape") handleClose()
-      if (e.key === "Tab" && last.current === document.activeElement) first.current.focus()
-      if (e.key === "Tab" && first.current === document.activeElement && shifted.current) last.current.focus()
-    }
-
-    first.current.focus()
-    background.addEventListener('keydown', handleKeyDown)
-    lastLink.addEventListener('keydown', handleKeyDown)
-    background.addEventListener('keyup', handleKeyUp)
-
-    return () => {
-      background.removeEventListener('keydown', handleKeyDown)
-      lastLink.removeEventListener('keydown', handleKeyDown)
-      background.removeEventListener('keyup', handleKeyUp)
-    }
-  }, [handleClose])
-
-  return (
-    <div className="mobile-menu-container"
-      ref={container}
-      role="navigation"
-    >
-      <div className="mobile-menu" role="region">
-        <button
-          className="xburger-container"
-          ref={first}
-          tabIndex={0}
-          onClick={handleClose}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") handleClose()
-          }}
-        >
-
-          <Xburger />
-        </button>
-
-        <div className="content">
-          <div className="link-list">
-            <div className="link-container">
-              <Link to="/" className="link">
-                Home
-                  </Link>
-            </div>
-            {navlinks.map((elem, index) => {
-              if (elem[0].startsWith("http")) {
-                return <div key={elem[0]} className="link-container">
-                  <a href={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`} target="_blank" rel="noopener noreferrer" ref={navlinks.length === index + 1 ? last : undefined}>
-                    {elem[1]}
-                  </a>
-                </div>
-              } else {
-                return <div key={elem[0]} className="link-container">
-                  <Link to={elem[0]} className={`link${elem[0] === "/donate" ? " special" : ""}`} ref={navlinks.length === index + 1 ? last : undefined}>
-                    {elem[1]}
-                  </Link>
-                </div>
-              }
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const Footer = ({ data }) => (
   <footer className="footer">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,91 +1,102 @@
 import React, { useRef } from "react"
-import { Link, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import '../styles/index.scss'
+import '../styles/project-card.scss'
+import '../styles/tech-card.scss'
 
+import Image from "gatsby-image"
 import useAnimateOnVisible from "../hooks/useAnimateOnVisible"
+import useMediaQuery from "../hooks/useMediaQuery"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Dogear from "../svg/dogear.svg"
+import Websites from "../svg/websites.svg"
+import Apps from "../svg/apps.svg"
+import Ecommerce from "../svg/ecommerce.svg"
 
-const Index = ({ data, location }) => {
-  const posts = data.allMarkdownRemark.edges
-
-  const playerMessage = useRef(null)
-  const sponsorMessage = useRef(null)
-  useAnimateOnVisible({ element: playerMessage })
-  useAnimateOnVisible({ element: sponsorMessage })
-
-  const playerCard = useRef(null)
-  const sponsorCard = useRef(null)
-  const companyCard = useRef(null)
-  useAnimateOnVisible({ element: companyCard })
-  useAnimateOnVisible({ element: playerCard })
-  useAnimateOnVisible({ element: sponsorCard })
-
-  return (
-    <Layout location={location}>
-      <SEO
-        title="Home"
-
-      >
-        <meta name="og:image" content="/media/shew-logo.png" />
-        <meta name="twitter:image" content="/media/shew-logo.png" />
-        <meta name="twitter:image:alt" content="Adopt a Minor Leaguer Home Page" />
-      </SEO>
-      <section className="section-blog">
-        <h2>Our Blog</h2>
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug} className="post">
-              <header>
-                <h3 className="post-title">
-                  <Link to={node.fields.slug}>
-                    {title}
-                  </Link>
-                </h3>
-                <small className="post-date">{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </section>
-            </div>
-          )
-        })}
-        <div className="container-link-button">
-          <Link to="/blog" className="link-button">
-            Visit All Posts
-        </Link>
-        </div>
-      </section>
-    </Layout>
-  )
-}
-
-export default Index
-
-export const pageQuery = graphql`
+const Index = ({ location }) => {
+  const data = useStaticQuery(graphql`
   query IndexQuery {
     companyLogo: file(absolutePath: { regex: "/shew-logo.png/" }) {
       childImageSharp {
-        fixed(width: 90, height: 90) {
+        fixed(width: 150, height: 150) {
           ...GatsbyImageSharpFixed
         }
       }
     }
-    anthony: file(absolutePath: {regex: "/anthony-shew.png/"}) {
+    aamlLogo: file(absolutePath: { regex: "/aaml-logo.png/" }) {
       childImageSharp {
-        fixed(width: 90, height: 90) {
+        fixed(width: 150, height: 150) {
           ...GatsbyImageSharpFixed
         }
       }
     }
-    corrins: file(absolutePath: {regex: "/the-corrins.png/"}) {
+    mmsiLogo: file(absolutePath: { regex: "/mmsi-logo.png/" }) {
       childImageSharp {
-        fixed(width: 90, height: 90) {
+        fixed(width: 150, height: 150) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    flatbillLogo: file(absolutePath: { regex: "/flatbill-logo.png/" }) {
+      childImageSharp {
+        fixed(width: 150, height: 150) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    javascript: file(absolutePath: { regex: "/javascript.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    node: file(absolutePath: { regex: "/node.png/" }) {
+      childImageSharp {
+        fixed(width: 72, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    react: file(absolutePath: { regex: "/react.png/" }) {
+      childImageSharp {
+        fixed(width: 90, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    express: file(absolutePath: { regex: "/express.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    git: file(absolutePath: { regex: "/git.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    postgreSQL: file(absolutePath: { regex: "/postgreSQL.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    gatsby: file(absolutePath: { regex: "/gatsby.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    figma: file(absolutePath: { regex: "/figma.png/" }) {
+      childImageSharp {
+        fixed(width: 54, height: 80) {
           ...GatsbyImageSharpFixed
         }
       }
@@ -111,4 +122,184 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+  `)
+
+  const posts = data.allMarkdownRemark.edges
+
+  const playerMessage = useRef(null)
+  const sponsorMessage = useRef(null)
+  useAnimateOnVisible({ element: playerMessage })
+  useAnimateOnVisible({ element: sponsorMessage })
+
+  const playerCard = useRef(null)
+  const sponsorCard = useRef(null)
+  const companyCard = useRef(null)
+  useAnimateOnVisible({ element: companyCard })
+  useAnimateOnVisible({ element: playerCard })
+  useAnimateOnVisible({ element: sponsorCard })
+
+  return (
+    <Layout location={location}>
+      <SEO
+        title="Home"
+      >
+        <meta name="og:image" content="/media/shew-logo.png" />
+        <meta name="twitter:image" content="/media/shew-logo.png" />
+        <meta name="twitter:image:alt" content="Adopt a Minor Leaguer Home Page" />
+      </SEO>
+
+      <section className="section-mystery">
+        <p className="question-mark">?</p>
+        <p className="first-line">My True Identity?</p>
+        <p className="second-line">That's a Secret...</p>
+        <Dogear onClick={() => console.log('page turn')} />
+        <div className="border-mask"></div>
+      </section>
+
+      {/* <section className="section-interjections">
+        <Websites />
+        <Apps />
+        <Ecommerce />
+      </section> */}
+
+      <section className="section-projects">
+        <h2>My Favorite Missions So&nbsp;Far</h2>
+        <ProjectCard
+          bgColor="#012B5D"
+          textColor="#FFFFFF"
+          buttonColor="#F39116"
+          title="Adopt a Minor Leaguer"
+          imgSrc={data.aamlLogo.childImageSharp.fixed}
+          imgAlt="Adopt a Minor Leaguer Logo"
+          description="ADOPT A MINOR LEAGUER IS A NON-PROFIT DEDICATED TO THE IMPROVEMENT OF THE LIVES OF MINOR LEAGUE BASEBALL PLAYERS."
+          type="Site"
+          projectLink="https://adoptaminorleaguer.com"
+          githubLink="https://github.com/anthonyshew/adoptaplayer"
+        />
+        <ProjectCard
+          bgColor="#3D5C5C"
+          textColor="#FFFFFF"
+          buttonColor="#FFFFFF"
+          title="MMSI"
+          imgSrc={data.mmsiLogo.childImageSharp.fixed}
+          imgAlt="Mission Mechanical Services Inc. Logo"
+          description="MISSION MECHANICAL SERVICES INC. IS ONE OF THE MOST REPUTABLE HVAC COMPANIES IN SOUTHERN CALIFORNIA."
+          type="Site"
+          projectLink="https://missionmechanicalservices.com"
+          githubLink="https://github.com/anthonyshew/mmsi"
+        />
+        <ProjectCard
+          bgColor="#6EC202"
+          textColor="#000000"
+          buttonColor="#000000"
+          title="Flatbill Jersey Customizer"
+          imgSrc={data.flatbillLogo.childImageSharp.fixed}
+          imgAlt="Flatbill Baseball Logo"
+          description="THE FLATBILL JERSEY CUSTOMIZER ALLOWS YOU TO DESIGN YOUR OWN JERSEY AND ORDER IT AS QUICK AND EASY AS POSSIBLE!"
+          type="App"
+          projectLink="https://customizer.flatbillbaseball.com"
+          githubLink="https://github.com/anthonyshew/flatbill-customizer"
+        />
+      </section>
+
+      <section className="section-tech">
+        <h2>Tools in My Belt</h2>
+        <div className="flex-container">
+          <TechCard
+            title="Javascript"
+            imgSrc={data.javascript.childImageSharp.fixed}
+          />
+          <TechCard
+            title="Node"
+            imgSrc={data.node.childImageSharp.fixed}
+          />
+          <TechCard
+            title="React"
+            imgSrc={data.react.childImageSharp.fixed}
+          />
+          <TechCard
+            title="Express"
+            imgSrc={data.express.childImageSharp.fixed}
+          />
+          <TechCard
+            title="Git"
+            imgSrc={data.git.childImageSharp.fixed}
+          />
+          <TechCard
+            title={useMediaQuery("(max-width: 475px)") ? "Postgres" : "PostgreSQL"}
+            imgSrc={data.postgreSQL.childImageSharp.fixed}
+          />
+          <TechCard
+            title="Gatsby"
+            imgSrc={data.gatsby.childImageSharp.fixed}
+          />
+          <TechCard
+            title="Figma"
+            imgSrc={data.figma.childImageSharp.fixed}
+          />
+        </div>
+        <p className="more">And a few others: JWT, Netlify, Sass, GraphQL, GSAP, react-hook-form, and more...</p>
+      </section>
+
+      <section className="section-blog">
+        <h2>My Blog</h2>
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug} className="post">
+              <header>
+                <h3 className="post-title">
+                  <Link to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small className="post-date">{node.frontmatter.date}</small>
+              </header>
+              <section>
+                <p className="post-excerpt"
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </section>
+            </div>
+          )
+        })}
+        <div className="container-link-button">
+          <Link to="/blog" className="link-button">
+            Visit All Posts
+        </Link>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default Index
+
+const ProjectCard = ({ bgColor, textColor, buttonColor, title, imgSrc, imgAlt, description, type, projectLink, githubLink }) => (
+  <div className="project-card" style={{ backgroundColor: bgColor, color: textColor }}>
+    <h3>{title}</h3>
+    <div className="flex-container">
+      <div className="image-container">
+        <Image fixed={imgSrc} alt={imgAlt} />
+      </div>
+      <div className="other-container">
+        <p>{description}</p>
+        <div className="linkbuttons-container">
+          <a style={{ backgroundColor: buttonColor, color: bgColor }} href={projectLink} target="_blank" rel="noreferrer noopener">{type}</a>
+          <a style={{ backgroundColor: buttonColor, color: bgColor }} href={githubLink} target="_blank" rel="noreferrer noopener">Github</a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+)
+
+const TechCard = ({ imgSrc, title }) => (
+  <span className="tech-card">
+    <Image fixed={imgSrc} alt={title} />
+    <p>{title}</p>
+  </span>
+)
